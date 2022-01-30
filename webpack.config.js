58385d2ab539cd104c96a6d68d240753da4edc65
poyuaki <maomuhuangying090@gmail.com>
2022-01-30 13:42:36 +0900

    ビルド時の設定を変更

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,10 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-const modeEnv = process.env.MODE === 'production' ? 'production' : 'development';
 
 module.exports = {
   // 開発用の設定
-  mode: modeEnv,
+  mode: 'development',
 
   resolve: {
     extensions: ['.js', '.ts']
